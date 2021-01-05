69b519e1d1237933655b006939b6c4fba0bc28e9
John Dave Dalmao <jhav3dalx@gmail.com>
2021-01-05 15:34:57 +0800

    Initial Commit

---
+++ ContactCRUD/Form1.Designer.cs
@@ -0,0 +1,219 @@
+﻿
+namespace ContactCRUD {
+    partial class Form1 {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.dgvContacts = new System.Windows.Forms.DataGridView();
+            this.txtMobileNumber = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtAddress = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.txtSearch = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvContacts)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(28, 45);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Name:";
+            // 
+            // txtName
+            // 
+            this.txtName.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.txtName.Location = new System.Drawing.Point(158, 41);
+            this.txtName.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(240, 26);
+            this.txtName.TabIndex = 1;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btnSave.Location = new System.Drawing.Point(32, 230);
+            this.btnSave.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(112, 35);
+            this.btnSave.TabIndex = 4;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // dgvContacts
+            // 
+            this.dgvContacts.AllowUserToAddRows = false;
+            this.dgvContacts.AllowUserToDeleteRows = false;
+            this.dgvContacts.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.dgvContacts.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvContacts.Location = new System.Drawing.Point(439, 90);
+            this.dgvContacts.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.dgvContacts.Name = "dgvContacts";
+            this.dgvContacts.ReadOnly = true;
+            this.dgvContacts.Size = new System.Drawing.Size(388, 175);
+            this.dgvContacts.TabIndex = 7;
+            this.dgvContacts.DoubleClick += new System.EventHandler(this.dgvContacts_DoubleClick);
+            // 
+            // txtMobileNumber
+            // 
+            this.txtMobileNumber.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.txtMobileNumber.Location = new System.Drawing.Point(158, 90);
+            this.txtMobileNumber.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtMobileNumber.Name = "txtMobileNumber";
+            this.txtMobileNumber.Size = new System.Drawing.Size(240, 26);
+            this.txtMobileNumber.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(28, 95);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(119, 20);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Mobile Number:";
+            // 
+            // txtAddress
+            // 
+            this.txtAddress.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.txtAddress.Location = new System.Drawing.Point(158, 138);
+            this.txtAddress.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtAddress.Multiline = true;
+            this.txtAddress.Name = "txtAddress";
+            this.txtAddress.Size = new System.Drawing.Size(240, 69);
+            this.txtAddress.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(28, 143);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(72, 20);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Address:";
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btnDelete.Location = new System.Drawing.Point(161, 230);
+            this.btnDelete.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(112, 35);
+            this.btnDelete.TabIndex = 5;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnReset
+            // 
+            this.btnReset.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btnReset.Location = new System.Drawing.Point(286, 230);
+            this.btnReset.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(112, 35);
+            this.btnReset.TabIndex = 6;
+            this.btnReset.Text = "Reset";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // txtSearch
+            // 
+            this.txtSearch.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.txtSearch.Location = new System.Drawing.Point(507, 42);
+            this.txtSearch.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtSearch.Name = "txtSearch";
+            this.txtSearch.Size = new System.Drawing.Size(320, 26);
+            this.txtSearch.TabIndex = 8;
+            this.txtSearch.KeyUp += new System.Windows.Forms.KeyEventHandler(this.txtSearch_KeyUp);
+            // 
+            // label4
+            // 
+            this.label4.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(435, 44);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(64, 20);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Search:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(863, 308);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtSearch);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.txtAddress);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtMobileNumber);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.dgvContacts);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Basic CRUD (With Stored Procedures)";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvContacts)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.DataGridView dgvContacts;
+        private System.Windows.Forms.TextBox txtMobileNumber;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtAddress;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.TextBox txtSearch;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ ContactCRUD/Form1.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace ContactCRUD {
+    public partial class Form1 : Form {
+        SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=D:\Visual Studio\ContactCRUD\ContactCRUD\Data\ContactDB.mdf;Integrated Security=True;Connect Timeout=30");
+        int ContactId = 0;
+
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e) {
+            FillDataGridView();
+            Reset();
+        }
+
+        void FillDataGridView() {
+            if (con.State == ConnectionState.Closed) {
+                con.Open();
+            }
+            //When retrieving data: Use SqlDataAdapter object
+            SqlDataAdapter sda = new SqlDataAdapter("ContactViewOrSearch", con);
+            sda.SelectCommand.CommandType = CommandType.StoredProcedure;
+            sda.SelectCommand.Parameters.AddWithValue("@ContactName", txtSearch.Text.Trim());
+            DataTable dt = new DataTable();
+            sda.Fill(dt);
+            dgvContacts.DataSource = dt;
+            dgvContacts.Columns[0].Visible = false;
+            con.Close();
+        }
+
+        void Reset() {
+            txtName.Text = txtMobileNumber.Text = txtAddress.Text = "";
+            btnSave.Text = "Save";
+            ContactId = 0;
+            btnDelete.Enabled = false;
+        }
+
+        private void btnSave_Click(object sender, EventArgs e) {
+            try {
+                if (con.State == ConnectionState.Closed) {
+                    con.Open();
+                }
+                if (btnSave.Text == "Save") {
+                    //When inserting data: Use SqlCommand object
+                    SqlCommand cmd = new SqlCommand("ContactAddOrEdit", con);
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@Mode", "Add");
+                    cmd.Parameters.AddWithValue("@ContactId", 0);
+                    cmd.Parameters.AddWithValue("@Name", txtName.Text.Trim());
+                    cmd.Parameters.AddWithValue("@MobileNumber", txtMobileNumber.Text.Trim());
+                    cmd.Parameters.AddWithValue("@Address", txtAddress.Text.Trim());
+                    cmd.ExecuteNonQuery();
+                    MessageBox.Show("Saved Successfully!", "Save");
+                }
+                else {
+                    SqlCommand cmd = new SqlCommand("ContactAddOrEdit", con);
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@Mode", "Edit");
+                    cmd.Parameters.AddWithValue("@ContactId", ContactId);
+                    cmd.Parameters.AddWithValue("@Name", txtName.Text.Trim());
+                    cmd.Parameters.AddWithValue("@MobileNumber", txtMobileNumber.Text.Trim());
+                    cmd.Parameters.AddWithValue("@Address", txtAddress.Text.Trim());
+                    cmd.ExecuteNonQuery();
+                    MessageBox.Show("Updated Successfully!", "Save");
+                }
+            }
+            catch (Exception ex) {
+                MessageBox.Show(ex.Message, "Error Message!");
+            }
+            finally {
+                con.Close();
+                FillDataGridView();
+            }
+        }
+
+        private void txtSearch_KeyUp(object sender, KeyEventArgs e) {
+            try {
+                FillDataGridView();
+            }
+            catch (Exception ex) {
+                MessageBox.Show(ex.Message, "Error Message!");
+            }
+        }
+
+        private void dgvContacts_DoubleClick(object sender, EventArgs e) {
+            if (dgvContacts.CurrentRow.Index > -1) {
+                ContactId = Convert.ToInt32(dgvContacts.CurrentRow.Cells[0].Value.ToString());
+                txtName.Text = dgvContacts.CurrentRow.Cells[1].Value.ToString();
+                txtMobileNumber.Text = dgvContacts.CurrentRow.Cells[2].Value.ToString();
+                txtAddress.Text = dgvContacts.CurrentRow.Cells[3].Value.ToString();
+                btnSave.Text = "Update";
+                btnDelete.Enabled = true;
+            }
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e) {
+            try {
+                if (con.State == ConnectionState.Closed) {
+                    con.Open();
+                }
+                //When inserting data: Use SqlCommand object
+                SqlCommand cmd = new SqlCommand("ContactDeletion", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@ContactId", ContactId);
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("Deleted Successfully!", "Save");
+                Reset();
+                FillDataGridView();
+            }
+            catch (Exception ex) {
+                MessageBox.Show(ex.Message, "Error Message!");
+            }
+        }
+
+        private void btnReset_Click(object sender, EventArgs e) {
+            Reset();
+        }
+
+    }
+}
